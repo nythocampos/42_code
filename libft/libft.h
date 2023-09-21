67a6169c458d114aae903f100f196c45f58c8892
nythocampos <nythocampos@gmail.com>
2023-09-21 18:34:35 +0200

    DONE: ft_memchr.c

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: antcampo <antcampo@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/13 12:58:46 by antcampo          #+#    #+#             */
-/*   Updated: 2023/09/21 17:54:37 by antcampo         ###   ########.fr       */
+/*   Updated: 2023/09/21 18:21:01 by antcampo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,7 @@ int		ft_tolower(int c);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
-//TODO: memchr
+void	*ft_memchr(const void *s, int c, size_t n);
 //TODO: memcmp
 //TODO: strnstr
 //TODO: atoi
