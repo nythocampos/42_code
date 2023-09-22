94af0168242cca96da245a301db3c27a35fcd60a
nythocampos <nythocampos@gmail.com>
2023-09-22 23:08:49 +0200

    DONE: ft_substr.c

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: antcampo <antcampo@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/22 18:30:15 by antcampo          #+#    #+#             */
-/*   Updated: 2023/09/22 22:05:19 by antcampo         ###   ########.fr       */
+/*   Updated: 2023/09/22 23:06:28 by antcampo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,12 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	int		index;
 	char	*str;
 
-	str = (char *)ft_calloc(3, sizeof(char));
+	str = (char *)ft_calloc(1, sizeof(char));
 	index = 0;
-	if (len > ft_strlen(s))
+	if (len >= ft_strlen(s))
 		len = ft_strlen(s) - (size_t) start;
 	if ((int) start > (int) ft_strlen(s) 
-		|| (int) ft_strlen(s) == 0)
+		|| (int) ft_strlen(s) == 0 || (int) start < 0 || (int) len < 0)
 		return (str);
 	free(str);
 	str = (char *)ft_calloc(len + 1, sizeof(char));
@@ -41,19 +41,16 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 /*
 int	main(void)
 {
-	char text[] = "lorem ipsum dolor sit amet";
-	size_t max = 18446744073709;
 	char *result;
 
-	result = ft_substr("tripouille", 1, 1);
+	result = ft_substr("hola", -1, -1);
 	printf("result --->%s\n", result);
 	printf("size: %ld\n", strlen(result));
-	printf("size: %ld\n", strlen(result));
-	if (ft_strlen(result) == 2)
-		printf("OK\n");
 	// if (len > ft_strlen(s))
 	// 	len = ft_strlen(s) - (int) start;
 	//str = (char *)ft_calloc(len + 1, sizeof(char));
 	//str = (char *)malloc(len * sizeof(char) + 1);
+	//printf("%u\n",start);
+	//printf("%zu\n",len);
 	return (0);
 }*/
