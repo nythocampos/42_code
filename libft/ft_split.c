e3fd084635a0b019c4809a4118f20919117e186f
nythocampos <nythocampos@gmail.com>
2023-09-24 03:12:11 +0200

    WORKING: ft_split.c num of rows found, array of strings working, working on details

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,12 +6,31 @@
 /*   By: antcampo <antcampo@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/23 22:38:40 by antcampo          #+#    #+#             */
-/*   Updated: 2023/09/24 01:43:59 by antcampo         ###   ########.fr       */
+/*   Updated: 2023/09/24 03:10:55 by antcampo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdio.h>
+
+static int	get_array_size(char const *s, char c)
+{
+	int	index;
+	int	rows;
+
+	index = 1;
+	rows = 0;
+	while (1)
+	{
+		if ((s[index] == c || s[index] == '\0') 
+			&& (s[index - 1] != c))
+			rows++;
+		if (s[index] == '\0')
+			break ;
+		index++;
+	}
+	return (rows);
+}
 
 static int	find_pos(char const *s, int start, int *len, char c)
 {
@@ -21,7 +40,7 @@ static int	find_pos(char const *s, int start, int *len, char c)
 	*len = 0;
 	while (s[index] != '\0')
 	{
-		if (s[index] == c)
+		if (s[index] == c && (s[index - 1] != c))
 			return (index - 1);
 		*len = *len + 1;
 		index++;
@@ -30,15 +49,13 @@ static int	find_pos(char const *s, int start, int *len, char c)
 	return (index - 1);
 }
 
-char	**ft_split(char const *s, char c)
+char	*get_substr(char const *s, char c)
 {
 	int		start;
 	int		end;
 	int		len;
 	char	*str;
 
-	start = 0;
-	end = 0;
 	while (start < ft_strlen(s))
 	{
 		end = find_pos(s, start, &len, c);
@@ -48,14 +65,51 @@ char	**ft_split(char const *s, char c)
 		free(str);
 		start = end + 2;
 	}
+	return (str);
+}
 
-	return ((char **) '\0');
+char	**ft_split(char const *s, char c)
+{
+	int		index;
+	int		start;
+	int		end;
+	int		len;
+	char	**str;
+
+	index = 0;
+	start = 0;
+	end = 0;
+	if (ft_strlen(s) == 0 || c == '\0')
+		return ((char **)ft_calloc(1, sizeof(char)));
+	str = (char **)ft_calloc(get_array_size(s, c), sizeof(char *));
+	while (start < ft_strlen(s) && index <= get_array_size(s, c))
+	{
+		end = find_pos(s, start, &len, c);
+		printf("start: %d, end: %d, len: %d ", start, end, len);
+		str[index] = ft_substr(s, start, len);
+		printf("--->%s\n", str[index]);
+		start = end + 2;
+		index++;
+	}
+	printf(" ----- \n");
+	return (str);
 }
 
 int	main(void)
 {
-	char text[] = "perro cor tej";
+	//char const text[] = "perro cor tej";
+	char const text[] = "perro cor tej    asd";
+	char	**result;
+	char	c = ' ';
+	int		rows = get_array_size(text, c);
+	int		i;
+	int		j;
 
-	ft_split(text, ' ');
+	//get_substr(text, c);
+	result = ft_split(text, c);
+	for (i=0; i<rows; i++)
+		printf("-->%s\n", result[i]);
+	//printf("%d \n", get_array_size(text, c));
+	free(result);
 	return (0);
 }
