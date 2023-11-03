1db5f0a61b745a4e30066dd5edf75a86a1dba47e
ThonyNards <nythocampos@gmail.com>
2023-11-03 18:16:37 +0100

    UPDATE: tests updated

--- get_next_line/test.c
+++ get_next_line/test.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include "get_next_line.h"
 #include <fcntl.h>
-
+/*
 int main(void)
 {
 	int		fd;
@@ -11,13 +11,13 @@ int main(void)
 	fd = open("test_file", O_RDONLY);
   result = "";
 	//use a loop to test
-  /*
+  
 	printf("1 Result------>%s\n", get_next_line(fd));
 	printf("2 Result------>%s\n", get_next_line(fd));
 	printf("3 Result------>%s\n", get_next_line(fd));
 	printf("4 Result------>%s\n", get_next_line(fd));
 	printf("5 Result------>%s\n", get_next_line(fd));
-  return (0); */
+  return (0);
 	while(result != NULL)
 	{
 		result = get_next_line(fd);
@@ -27,7 +27,7 @@ int main(void)
 	close(fd);
 	return (0);
 }
-
+*/
 /*int	main(void)
 {
 	printf("%d", BUFFER_SIZE);
