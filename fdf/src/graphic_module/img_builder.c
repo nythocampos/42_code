849fadebb42a7fde146b99831bbd65ab01cd6f2e
ThonyNards <nythocampos@gmail.com>
2024-03-01 20:05:47 +0100

    UPDATED: added draw_line function

--- fdf/src/graphic_module/img_builder.c
+++ fdf/src/graphic_module/img_builder.c
@@ -23,37 +23,115 @@ static void	set_pixel(t_img *img, int x, int y, int color)
 	*((unsigned int*)(offset + img->addr)) = color;
 }
 
+static void	draw_line(t_mlx_data *mlx, t_s_cor *a, t_s_cor *b)
+{
+	float	dy;
+	float	dx;
+	int	step_x;
+	int	step_y;
+	int	temp_x;
+	int	temp_y;
+	float	p;
+
+	dy = b->y - a->y;
+	dx = b->x - a->x;
+	if (dx > 0){
+		step_x = 1;
+	} else {
+		step_x = -1;
+		dx = -dx;
+	}
+	if (dy > 0) {
+		step_y = 1;
+	} else {
+		step_y = -1;
+		dy = -dy;
+	}
+
+	// Draw pixels between two points
+	temp_x = a->x;
+	temp_y = a->y;
+
+	if (dx >= dy)
+	{
+		p = 2 * dy - dx;
+		while (temp_x != b->x)
+		{
+			set_pixel(
+				mlx->img,
+				temp_x,
+				temp_y,
+				COLOR_B);
+			temp_x = temp_x + step_x;
+			if (p >= 0)
+			{
+				temp_y = temp_y + step_y;
+				p = p - (2 * dx);
+			}
+			p = p + (2 * dy);
+		}
+	}
+	else
+	{
+		p = 2 * dx - dy;
+		while (temp_y != b->y)
+		{
+			set_pixel(
+				mlx->img,
+				temp_x,
+				temp_y,
+				COLOR_B);
+			temp_y = temp_y + step_y;
+			if (p >= 0)
+			{
+				temp_x = temp_x + step_x;
+				p = p - (2 * dy);
+			}
+			p = p + (2 * dx);
+		}
+	}
+}
+
+static void	draw_face(t_mlx_data *mlx_data, t_s_cor *points)
+{
+	int	pts_i;
+	int	end_pts;
+	t_s_cor	*a;
+	t_s_cor	*b;
+
+	pts_i = 0;
+	end_pts = 0;
+	while(end_pts == 0)
+	{
+		a = &points[pts_i];
+
+		if (points[pts_i].id == -1)
+		{
+			b = &points[0];
+			end_pts = 1;
+		}
+		else 
+		{
+			b = &points[pts_i + 1];
+		}
+		pts_i++;
+		draw_line(mlx_data, a, b);
+	}
+}
+
 /* 
  * This function draw the faces 
  * */
 static void	draw_model(t_mlx_data *mlx_data, t_face *faces_lst)
 {
 	int	faces_i;
-	int	pts_i;
 	int	end_faces;
-	int	end_pts;
-	t_s_cor	*points;
 
 	faces_i = 0;
-	pts_i = 0;
 	end_faces = 0;
-	end_pts = 0;
 	while(end_faces == 0)
 	{
-		points = faces_lst[faces_i].points;
-		while(end_pts == 0)
-		{	
-			set_pixel(
-				mlx_data->img,
-				points[pts_i].x,
-				points[pts_i].y,
-				COLOR_B);
-			pts_i++;
-			if (points[pts_i].id == -1)
-				end_pts = 1;
-		}
-		pts_i = 0;
-		end_pts = 0;
+		draw_face(mlx_data, &faces_lst[faces_i].points[0]);
 		faces_i++;
 		if (faces_lst[faces_i].id == -1)
 			end_faces = 1;
